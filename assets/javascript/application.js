b2ee1cd6ee3b9944b26fdb8847163e3d38ffbb09
Luis Puente <puente.luism@gmail.com>
2020-03-17 11:49:02 -0600

    add Read button

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -11,13 +11,19 @@ Book.prototype.createBtn = function(cont) {
   var x = document.createElement("input");
   x.setAttribute("type", "button");
   x.setAttribute('id', `read-${cont}`);
-  x.setAttribute('value', '&#10004;');
-  x.setAttribute('class', 'btn btn-danger btn-success')
+  x.setAttribute('value', '✔️');
+  x.setAttribute('class', 'read-btn btn btn-danger btn-success')
   return x;
 };
 
-Book.prototype.readBook = function(e){
-  e.addEventListener('click', classList.toggle('btn-danger'))};
+/*const readBook = function(event) {
+  console.log(event.currentTarget)
+};
+*/
+document.addEventListener('click', function(e) {
+  console.log(e.currentTarget);
+  console.log(this);
+});
 
 function addBookToLibrary(newBook) {
   return myLibrary.push(newBook);
@@ -42,7 +48,8 @@ addBookToLibrary(book1);
 let cont = 0;
 
 template = myLibrary.map(
-    x => `<tr><td>${x.title}</td><td>${x.author}</td><td>${x.pages}</td><td>${x.createBtn(cont)}</td><td><input type="button" value="delete" class="btn btn-secondary" id="delete-${cont ++}"></td></tr>`
+    x => `<tr><td>${x.title}</td><td>${x.author}</td><td>${x.pages}</td><td>${x.createBtn(cont).outerHTML}</td><td><input type="button" value="delete" class="btn btn-secondary" id="delete-${cont ++}"></td></tr>`
   );
 
 render(template.join(''), document.querySelector('#table-content'));
+console.log(myLibrary.map(x => x.readBook))
