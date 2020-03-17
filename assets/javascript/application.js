e5fe6247d83f5d723b79526b18caba27f8da6850
lucasmazo32 <lucasmazo32@gmail.com>
2020-03-17 12:09:16 -0500

    added more functions for the button creation, solved minor bug with the toggle class

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -7,6 +7,18 @@ function Book(title, author, pages, have_read) {
   this.have_read = have_read
 };
 
+Book.prototype.createBtn = function(cont) {
+  var x = document.createElement("input");
+  x.setAttribute("type", "button");
+  x.setAttribute('id', `read-${cont}`);
+  x.setAttribute('value', '&#10004;');
+  x.setAttribute('class', 'btn btn-danger btn-success')
+  return x;
+};
+
+Book.prototype.readBook = function(e){
+  e.addEventListener('click', classList.toggle('btn-danger'))};
+
 function addBookToLibrary(newBook) {
   return myLibrary.push(newBook);
 };
@@ -15,19 +27,12 @@ let render = function (template, node) {
 	node.innerHTML = template;
 };
 
+let createBtn = document.getElementById('create-new');
+let debbug = document.getElementById('debbug');
 
-/*const newBook = document.querySelector('.new-book')
-const newForm = document.querySelector('.new-form')
-newBook.addEventListener("click", 
-newForm.classList.toggle('d-none')
-*/
-let createBtn = document.getElementById('create-new'); 
-let debbug = document.getElementById('debbug'); 
-let formSubmit = document.getElementById('form-submit'); 
-createBtn.onclick = function() { 
-  let bookForm = document.getElementById('book-form'); 
-  if (bookForm.style.display == 'none'){ bookForm.style.display = 'flex' } 
-  else { bookForm.style.display = 'none' } }; 
+createBtn.onclick = function() {
+  let bookForm = document.getElementById('book-form');
+  bookForm.classList.toggle('d-none')};
 
 const book1 = new Book('Hobbit', 'JRR', 260, false);
 addBookToLibrary(book1);
@@ -37,7 +42,7 @@ addBookToLibrary(book1);
 let cont = 0;
 
 template = myLibrary.map(
-    x => `<tr><td>${x.title}</td><td>${x.author}</td><td>${x.pages}</td><td></td><td><input type="button" value="delete" id="delete-${cont ++}"></td></tr>`
+    x => `<tr><td>${x.title}</td><td>${x.author}</td><td>${x.pages}</td><td>${x.createBtn(cont)}</td><td><input type="button" value="delete" class="btn btn-secondary" id="delete-${cont ++}"></td></tr>`
   );
 
 render(template.join(''), document.querySelector('#table-content'));
