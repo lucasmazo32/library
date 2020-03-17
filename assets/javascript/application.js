f612081428c462e06784b6720ac679434e676aad
Luis Puente <puente.luism@gmail.com>
2020-03-17 10:34:11 -0600

    Add form for new book

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -15,6 +15,20 @@ let render = function (template, node) {
 	node.innerHTML = template;
 };
 
+
+/*const newBook = document.querySelector('.new-book')
+const newForm = document.querySelector('.new-form')
+newBook.addEventListener("click", 
+newForm.classList.toggle('d-none')
+*/
+let createBtn = document.getElementById('create-new'); 
+let debbug = document.getElementById('debbug'); 
+let formSubmit = document.getElementById('form-submit'); 
+createBtn.onclick = function() { 
+  let bookForm = document.getElementById('book-form'); 
+  if (bookForm.style.display == 'none'){ bookForm.style.display = 'flex' } 
+  else { bookForm.style.display = 'none' } }; 
+
 const book1 = new Book('Hobbit', 'JRR', 260, false);
 addBookToLibrary(book1);
 addBookToLibrary(book1);
