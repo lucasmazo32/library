ccdb4d71a710c8884025e77cffe6adbdaa8388cf
Luis Puente <puente.luism@gmail.com>
2020-03-20 08:56:30 -0600

    Add validations and form clear

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -34,7 +34,7 @@ function addBookToLibrary(newBook) {
 const render = (template, node) => { node.innerHTML = template; };
 
 const createdBtn = document.getElementById('create-new');
-
+const actualForm = document.querySelector('.new-book-form')
 createdBtn.onclick = () => {
   const bookForm = document.getElementById('book-form');
   bookForm.classList.toggle('d-none');
@@ -63,29 +63,37 @@ const markRead = document.querySelectorAll('.mark-read');
 const deleteItem = document.querySelectorAll('.mark-delete');
 
 newEntry.addEventListener('click', () => {
-  const x = new Book(field1.value, field2.value, field3.value, field4.checked);
-  const newRow = document.createElement('TR');
-  newRow.innerHTML = `<td>${x.title}</td><td>${x.author}</td><td>${
-    x.pages
-  }</td><td>${x.createdBtn(cont, readIt).outerHTML}</td><td>${
-    x.createdBtn(cont, deleteIt).outerHTML
-  }</td>`;
-  table.appendChild(newRow);
-  cont += 1;
+  if (field1.value.length !== 0 && field2.value.length !== 0 && field3.value !== '') {
+    const x = new Book(field1.value, field2.value, field3.value, field4.checked);
+    const newRow = document.createElement('TR');
+    newRow.innerHTML = `<td>${x.title}</td><td>${x.author}</td><td>${
+      x.pages
+    }</td><td>${x.createdBtn(cont, readIt).outerHTML}</td><td>${
+      x.createdBtn(cont, deleteIt).outerHTML
+    }</td>`;
+    table.appendChild(newRow);
+    cont += 1;
+    actualForm.reset();
 
-  const readbtn = document.querySelector(`#read-${cont - 1}`);
-  const deletebtn = document.querySelector(`#delete-${cont - 1}`);
-  if (field4.checked) {
-    readbtn.classList.toggle('btn-danger');
+    const readbtn = document.querySelector(`#read-${cont - 1}`);
+    const deletebtn = document.querySelector(`#delete-${cont - 1}`);
+    if (field4.checked) {
+      readbtn.classList.toggle('btn-danger');
+    }
+    readbtn.addEventListener('click', function foo() {
+      this.classList.toggle('btn-danger');
+    });
+
+    deletebtn.addEventListener('click', function foo() {
+      const upEle = this.parentElement.parentElement;
+      upEle.remove();
+    });
+  } else {
+    const warn = document.createElement('P');
+    warn.setAttribute('class', 'bg-warning p-3 mt-3')
+    warn.innerHTML = 'Great job, however, there are some changes that need to be implemented before moving on: make sure you fill out all fields before resubmitting';
+    actualForm.appendChild(warn);
   }
-  readbtn.addEventListener('click', function foo() {
-    this.classList.toggle('btn-danger');
-  });
-
-  deletebtn.addEventListener('click', function foo() {
-    const upEle = this.parentElement.parentElement;
-    upEle.remove();
-  });
 });
 
 markRead.forEach(button => {
